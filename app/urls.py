33fc591ae805d004909a0dfaa84f2a8c4e0a728e
Muhammad <muhammadrahmatulloev540@gmail.com>
2026-04-12 22:02:21 +0500

    added style to htmls in css

--- app/urls.py
+++ app/urls.py
@@ -14,17 +14,17 @@ urlpatterns = [
     path('authors/<int:pk>/', views.author_detail_view, name='author_detail'),
     path('authors/create/', views.author_create_view, name='author_create'),
     path('authors/<int:pk>/update/', views.author_update_view, name='author_update'),
-    path('author/<int:pk>/delete/', views.author_delete_view, name='author_delete'),
+    path('authors/<int:pk>/delete/', views.author_delete_view, name='author_delete'),
 
-    path('publisher/', views.publisher_list_view, name='publisher_list'),
-    path('publisher/<int:pk>/', views.publisher_detail_view, name='publisher_detail'),
-    path('publisher/create/', views.publisher_create_view, name='publisher_create'),
-    path('publisher/<int:pk>/update/', views.publisher_update_view, name='publisher_update'),
-    path('publisher/<int:pk>/delete/', views.publisher_delete_view, name='publisher_delete'),
+    path('publishers/', views.publisher_list_view, name='publisher_list'),
+    path('publishers/<int:pk>/', views.publisher_detail_view, name='publisher_detail'),
+    path('publishers/create/', views.publisher_create_view, name='publisher_create'),
+    path('publishers/<int:pk>/update/', views.publisher_update_view, name='publisher_update'),
+    path('publishers/<int:pk>/delete/', views.publisher_delete_view, name='publisher_delete'),
 
-    path('book/', views.book_list_view, name='book_list'),
-    path('book/<int:pk>/', views.book_detail_view, name='book_detail'),
-    path('book/create/', views.book_create_view, name='book_create'),
-    path('book/<int:pk>/update/', views.book_update_view, name='book_update'),
-    path('book/<int:pk>/delete', views.book_delete_view, name='book_delete')
+    path('books/', views.book_list_view, name='book_list'),
+    path('books/<int:pk>/', views.book_detail_view, name='book_detail'),
+    path('books/create/', views.book_create_view, name='book_create'),
+    path('books/<int:pk>/update/', views.book_update_view, name='book_update'),
+    path('books/<int:pk>/delete/', views.book_delete_view, name='book_delete')
 ]
